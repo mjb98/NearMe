7f87d4d97f899b58dcb6304fcad186749d052fbe
MJB <mjb1377@gmail.com>
2021-05-04 15:16:06 +0430

    Initial Commit

---
+++ NearMe/NearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeApp.swift
+//  NearMe
+//
+//  Created by Mohammad Javad Bashtani on 2/14/1400 AP.
+//
+
+import SwiftUI
+
+@main
+struct NearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
